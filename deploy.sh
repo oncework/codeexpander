7b8405db215eb5564b52f588229e9f80de91beed
xudaolong <xudaolong@yy.com>
2020-03-13 12:42:21 +0800

    chore: 更新脚本

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,14 @@ npm run build
 # 进入生成的文件夹
 cd public
 
+# 如果是发布到自定义域名
+echo 'codeexpander.com' > CNAME
+
 git init
 git remote add coding https://e.coding.net/xudaolong/codeexpander.git
 git checkout -b gh-pages-source
 git add -A
 git commit -m 'deploy'
 git push coding gh-pages-source -f
-
-# 如果是发布到自定义域名
-echo 'codeexpander.com' > CNAME
 
 cd -
